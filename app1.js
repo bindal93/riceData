ddb1b2c007b57c096333f21b6fd02af69db3f8d1
shivambindal93 <shivam.bindal@nagarro.com>
2023-07-27 11:53:44 +0530

    update code

--- app1.js
+++ app1.js
@@ -20,52 +20,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   port: 5432 // Default PostgreSQL port
 // });
 
-//prod PostgreSQL connection pool configuration
-const pool = new Pool({
-  user: "shivam",
-  host: "34.69.190.106",
-  database: "rice",
-  password: "shivam",
-  port: 5432 // Default PostgreSQL port
-});
+// //prod PostgreSQL connection pool configuration
+// const pool = new Pool({
+//   user: "shivam",
+//   host: "34.69.190.106",
+//   database: "rice",
+//   password: "shivam",
+//   port: 5432 // Default PostgreSQL port
+// });
 
 // Endpoint to render the index page
 app.get("/", (req, res) => {
   res.render("index", { rows: [] });
 });
 
-// Endpoint to handle form submission
-app.post("/submit", multer().single("image"), async (req, res) => {
-  const data = JSON.parse(req.body.data);
-  console.log(data);
-  // Validate data and calculate total percentage
-  let totalPercentage = 0;
-  for (const item of data) {
-    if (isNaN(item.percentage) || item.percentage <= 0) {
-      return res.status(400).json({ error: "Invalid data. Please enter valid percentage values." });
-    }
-    totalPercentage += parseInt(item.percentage, 10);
-  }
+// // Endpoint to handle form submission
+// app.post("/submit", multer().single("image"), async (req, res) => {
+//   const data = JSON.parse(req.body.data);
+//   console.log(data);
+//   // Validate data and calculate total percentage
+//   let totalPercentage = 0;
+//   for (const item of data) {
+//     if (isNaN(item.percentage) || item.percentage <= 0) {
+//       return res.status(400).json({ error: "Invalid data. Please enter valid percentage values." });
+//     }
+//     totalPercentage += parseInt(item.percentage, 10);
+//   }
 
-  if (totalPercentage !== 100) {
-    return res.status(400).json({ error: "Total percentage should be 100." });
-  }
+//   if (totalPercentage !== 100) {
+//     return res.status(400).json({ error: "Total percentage should be 100." });
+//   }
 
-  // Insert data and image into the PostgreSQL database
-  const query = "INSERT INTO rice_categories (data, image) VALUES ($1, $2)";
+//   // Insert data and image into the PostgreSQL database
+//   const query = "INSERT INTO rice_categories (data, image) VALUES ($1, $2)";
 
-  try {
-    const client = await pool.connect();
+//   try {
+//     const client = await pool.connect();
 
-    // Insert data and image as a single row in the rice_categories table
-    await client.query(query, [JSON.stringify(data), req.file.buffer]);
+//     // Insert data and image as a single row in the rice_categories table
+//     await client.query(query, [JSON.stringify(data), req.file.buffer]);
 
-    client.release(); // Release the client back to the pool
-    return res.json({ message: "Data inserted successfully." });
-  } catch (error) {
-    return res.status(500).json({ error: "Error inserting data into the database." });
-  }
-});
+//     client.release(); // Release the client back to the pool
+//     return res.json({ message: "Data inserted successfully." });
+//   } catch (error) {
+//     return res.status(500).json({ error: "Error inserting data into the database." });
+//   }
+// });
 
 // Start the server
 const port = 3000;
